9899454f2784b97ae72dd825a31442ff9e9ca195
OdyssMorgan <odyssmorgan732@gmail.com>
2026-03-08 13:08:40 -0700

    Added sanitized input function

--- labs/lab_1/lab_1b.py
+++ labs/lab_1/lab_1b.py
@@ -7,8 +7,7 @@ then performs the operation and returns the result.
 The script asks the user to input the numbers and the operation to be performed,
 and prints the result to the terminal window.
 
-"""
-
+""" 
 def simple_calculator(operation: str, num1: float, num2: float) -> float:
     """
     Function that takes in two numbers and an operation (add, subtract, multiply, divide),
@@ -36,14 +35,22 @@ def simple_calculator(operation: str, num1: float, num2: float) -> float:
             raise ValueError("Cannot divide by zero.")
     else:
         raise ValueError("Invalid operation. Please choose from 'add', 'subtract', 'multiply', or 'divide'.")
+def sanitized_number_input(prompt: str) -> float:
+    while True:
+        try:
+            number = float(input(prompt))
+            return number
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
+            print("debug")
 
 def main():
     
     print(f"===== Simple Calculator =====")
 
     # Ask the user for sample input    
-    num1 = float(input("Enter the first number: "))
-    num2 = float(input("Enter the second number: "))
+    num1 = sanitized_number_input("Enter the first number: ")
+    num2 = sanitized_number_input("Enter the second number: ")
     operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
 
     # Perform the calculation and display the result
